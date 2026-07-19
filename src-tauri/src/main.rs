47448fc84631ed064be441226da06b0048061da5
Kheyox <foreval69@gmail.com>
2026-07-19 23:51:48 +0200

    feat(desktop): implement official Tauri signed auto-updater

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,6 +5,8 @@
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .run(tauri::generate_context!())
         .expect("erreur au lancement de Shiori");
 }
